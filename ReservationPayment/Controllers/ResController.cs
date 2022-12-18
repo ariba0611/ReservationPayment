5c96736fef27e14bb12e22fbd0cc71ab6792da04
ariba.siddiqui <aribasiddiqui111@gmail.com>
2022-12-18 15:16:26 +0530

    resc

--- ReservationPayment/Controllers/ResController.cs
+++ ReservationPayment/Controllers/ResController.cs
@@ -1,8 +1,6 @@
 ﻿using ReservationPayment.Models;
 using ReservationPayment.Models.DAL;
 using System.Collections.Generic;
-
-using System.Diagnostics;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -10,7 +8,6 @@ namespace ReservationPayment.Controllers
 {
     public class ResController : Controller
     {
-        List<Reservation> resList = new List<Reservation>();
         private IRailwayRepository<TrainDetails> TrainObj;
         public ResController()
         {
@@ -19,7 +16,7 @@ namespace ReservationPayment.Controllers
         }
         public ActionResult Index()
         {
-              List<Reservation> resList = new List<Reservation>();
+
             List<TrainDetails> trainDetails = TrainObj.GetModel().ToList();
 
             ViewData["TrainDetailS"] = new SelectList(trainDetails, dataValueField: "SourceStation", dataTextField: "SourceStation");
@@ -32,39 +29,25 @@ namespace ReservationPayment.Controllers
         public ActionResult Index(Reservation res)
 
         {
-           
-                 var data  = res ;
-                 resList.Add(data);
-                //Debug.WriteLine(resList[1]);
 
-                for(int i = 0; i < resList.Count(); i++)
-               {
-                Debug.WriteLine("Aditya" + resList[i]);
-               }
+            Session["ResName"] = res.Res_Name;
+            Session["ResGender"] = res.Res_Gender;
 
-         
-             Session["ResGender"] = res.Res_Gender;
-            
-                List<TrainDetails> trainDetails = TrainObj.GetModel().ToList();
+            List<TrainDetails> trainDetails = TrainObj.GetModel().ToList();
 
-               ViewData["TrainDetailS"] = new SelectList(trainDetails, dataValueField: "SourceStation", dataTextField:  "SourceStation");
-               ViewData["TrainDetailDesti"] = new SelectList(trainDetails,dataValueField: "DestinationStation",dataTextField: "DestinationStation");
+            ViewData["TrainDetailS"] = new SelectList(trainDetails, dataValueField: "SourceStation", dataTextField: "SourceStation");
+            ViewData["TrainDetailDesti"] = new SelectList(trainDetails, dataValueField: "DestinationStation", dataTextField: "DestinationStation");
 
-                Session["SourceStation"] = res.TrainDetails.SourceStation;
+            Session["SourceStation"] = res.TrainDetails.SourceStation;
 
-               Session["DestinationStation"] = res.TrainDetails.DestinationStation;
+            Session["DestinationStation"] = res.TrainDetails.DestinationStation;
 
 
 
 
-
-            return View(resList);
-
-        }
-
-        public ActionResult ListRes() {
 
             return View();
+
         }
 
     }
